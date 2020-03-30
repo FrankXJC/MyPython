5e05e824ce12529ac49d74d602c2d86e543d7fa3
frankXJC <1147296957@qq.com>
2020-03-30 15:24:07 +0800

    python example

---
+++ test01.py
@@ -0,0 +1,14 @@
+import time
+#time == 2
+print('hello')
+#time == 2
+print('I am Frank')
+#time == 3
+print('nice to meet you')
+language = input('English / 中文: ')
+if language == 'English':
+    print('study!')
+if language == '中文':
+    print('学习！')
+else:
+    print('🤓️')
